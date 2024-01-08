de3d58d5f65f5d2d2e6bed955a01f237a6579c06
Oliver Oldenburg <eolol002@edu.xamk.fi>
2024-01-08 11:53:02 +0200

    Second commit for the sake of commenting

--- FirstExercise-main/script.js
+++ FirstExercise-main/script.js
@@ -1,3 +1,14 @@
 document.getElementById("actionButton").addEventListener("click", function() {
     alert("Button clicked!");
 });
+
+function displayText(){
+    let text = document.getElementById("Text");
+    text.style.display ="block";
+}
+
+const date = new Date();
+let year = date.getFullYear();
+
+document.getElementById("year").innerHTML = year;
+
