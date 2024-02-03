e1d6a81acb9aa5e23de8b83a42e9b742bbe6adc7
OliverOldenburg <155956096+OliverOldenburg@users.noreply.github.com>
2024-02-03 19:50:25 +0200

    Updated main.jsx

--- Third Assignment More React/React-Vite-Assignment/src/main.jsx
+++ Third Assignment More React/React-Vite-Assignment/src/main.jsx
@@ -1,3 +1,5 @@
+// Imports all of the necessary libraries and addons
+
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import {
@@ -5,15 +7,51 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
-import Root from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact, {
+  loader as contactLoader,
+  action as contactAction,
+} from "./routes/contact";
+import Root, { loader as rootLoader } from "./routes/root";
+import EditContact, {
+  action as editAction,
+} from "./routes/edit";
+import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
+
+
+// Creates a router and adds its configurations to it
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      { index: true, element: <Index /> },
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+        action: contactAction,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
+      },
+    ],
   },
 ]);
 
+// This show the main application
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
