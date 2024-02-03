e623cffca558fef3623d62efdd02d619be24548f
OliverOldenburg <155956096+OliverOldenburg@users.noreply.github.com>
2024-02-03 19:54:29 +0200

    Updated root.jsx

--- Third Assignment More React/React-Vite-Assignment/src/routes/root.jsx
+++ Third Assignment More React/React-Vite-Assignment/src/routes/root.jsx
@@ -1,43 +1,127 @@
+import {
+  Outlet,
+  NavLink,
+  useLoaderData,
+  Form,
+  useNavigation,
+  useSubmit,
+  redirect,
+} from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+import { useEffect } from "react";
+
+// Creates a new contact
+export async function action() {
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+}
+
+// Grabs contacts from the query, based from the url
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q") || "";
+  const contacts = await getContacts(q);
+  return { contacts, q }
+}
+
+// Displays the sidebar
 export default function Root() {
-    return (
-      <>
-        <div id="sidebar">
-          <h1>React Router Contacts</h1>
-          <div>
-            <form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div
-                id="search-spinner"
-                aria-hidden
-                hidden={true}
-              />
-              <div
-                className="sr-only"
-                aria-live="polite"
-              ></div>
-            </form>
-            <form method="post">
-              <button type="submit">New</button>
-            </form>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <a href={`/contacts/1`}>Your Name</a>
-              </li>
-              <li>
-                <a href={`/contacts/2`}>Your Friend</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div id="detail"></div>
-      </>
+  const { contacts, q } = useLoaderData();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+
+  // Used to see if a search is ongoing
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has(
+      "q"
     );
-  }
+  useEffect(() => {
+    document.getElementById("q").value = q;
+  }, [q]);
+
+  
+
+  // Shows the contact list + button and search bar, all in a sidebar
+  return (
+    <>
+      <div id="sidebar">
+        <h1>React Router Contacts</h1>
+        <div>
+        <Form id="search-form" role="search">
+            <input
+              id="q"
+              className={searching ? "loading" : ""}
+              aria-label="Search contacts"
+              placeholder="Search"
+              type="search"
+              name="q"
+              defaultValue={q}
+              onChange={(event) => {
+                const isFirstSearch = q == null;
+                submit(event.currentTarget.form, {
+                  replace: !isFirstSearch,
+                });
+              }}
+              />
+            <div
+              id="search-spinner"
+              aria-hidden
+              hidden={!searching}
+            />
+            <div
+              className="sr-only"
+              aria-live="polite"
+            ></div>
+          </Form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </div>
+        
+         {/* Contact list */}
+        <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+        </nav>
+      </div>
+      {/* Detail section with Outlet for rendering nested routes */}
+      <div id="detail"
+        className={
+          navigation.state === "loading" ? "loading" : ""
+        }
+        >
+        <Outlet />
+      </div>
+    </>
+  );
+}
