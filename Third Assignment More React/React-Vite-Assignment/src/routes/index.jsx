93c5ada90f613cad37e1d4565941b1511df16d08
OliverOldenburg <155956096+OliverOldenburg@users.noreply.github.com>
2024-02-03 20:05:47 +0200

    Create index.jsx

---
+++ Third Assignment More React/React-Vite-Assignment/src/routes/index.jsx
@@ -0,0 +1,14 @@
+// Renders some basic text and adds a link to React router
+export default function Index() {
+    return (
+      <p id="zero-state">
+        This is a demo for React Router.
+        <br />
+        Check out{" "}
+        <a href="https://reactrouter.com">
+          the docs at reactrouter.com
+        </a>
+        .
+      </p>
+    );
+  }
