2bf5505030031ddf5b8643c83d503643baa734cb
Oliver Oldenburg <eolol002@edu.xamk.fi>
2024-02-18 20:55:08 +0200

    Started and finished!

--- Fifth Assignment Even More React/fifth-assignment/src/pages/NoPage.jsx
+++ Fifth Assignment Even More React/fifth-assignment/src/pages/NoPage.jsx
@@ -1,8 +1,7 @@
 
-// Function that brings you to an error page upon entering a nonexistant page.
-// Returns a message saying page couldn't be found.
+// Shows an error page should you somehow stumble into one.
 const NoPage = () => {
-    return <h3>Error 404: Page not found</h3>;
+    return <h3>Error 404: Page not found, please remain within the allowed walls of the website.</h3>;
   };
   
   export default NoPage;
